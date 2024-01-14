d1fb8a759efd5fba2a1fa5246b4fe4095ce2cd26
SinglePoi <rotate13@outlook.com.com>
2024-01-14 11:40:01 +0800

    ✨ feat(fiber): 实现任务调度器

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 <h1 align="center">mini-react</h1> 
-<p align="center"> 崔学社 react 学习记录 </p>
+<p align="center"> 渐进式 react 学习记录 </p>
 
 ### 结构
 
@@ -17,6 +17,7 @@
 ### 计划
 
 ✏️ Step1 实现最简 mini-react
+🎈 Step2 实现 fiber 架构
 
 ### 内容
 
@@ -29,6 +30,11 @@
 - 以 React 的方式渲染节点
 - 借助 Vite 提供 JSX 的支持
 
+🎈 Step2 在浏览器空闲时间处理 DOM 树结构过大的问题
+
+- 使用 requestIdleCallback API 实现将 DOM 树转换为链表结构
+
 ### 收获
 
 数据结构的装箱比拆箱更重要，好的结构可以减轻后续算法处理的心智负担
+requestIdleCallback API 接受一个函数，并在浏览器空闲时期执行，不影响关键事件
--- core/React.js
+++ core/React.js
@@ -1,4 +1,5 @@
 const textType = "TEXT_ELEMENT";
+let nextUnitOfWork = null;
 
 function createTextNode(text) {
   return {
@@ -23,25 +24,83 @@ function createElement(type, props, ...children) {
 }
 
 function render(el, container) {
-  const dom =
-    el.type === textType
-      ? document.createTextNode("")
-      : document.createElement(el.type);
+  // 描述容器
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [el],
+    },
+  };
+}
 
-  // props
-  Object.keys(el.props).forEach((key) => {
+function fiberLoop(deadline) {
+  let shouleYield = false;
+  while (!shouleYield && nextUnitOfWork) {
+    // todo run
+    nextUnitOfWork = performfiberOfUnit(nextUnitOfWork);
+
+    shouleYield = deadline.timeRemaining() < 1;
+  }
+
+  requestIdleCallback(fiberLoop);
+}
+
+function createDom(type) {
+  return type === textType
+    ? document.createTextNode("")
+    : document.createElement(type);
+}
+
+function updateProps(dom, props) {
+  Object.keys(props).forEach((key) => {
     if (key !== "children") {
-      dom[key] = el.props[key];
+      dom[key] = props[key];
     }
   });
-
-  // children
-  el.props.children.forEach((child) => {
-    render(child, dom);
-  });
-
-  container.append(dom);
 }
+
+function initChildren(fiber) {
+  const children = fiber.props.children;
+  let prevChild = null;
+  children.forEach((child, index) => {
+    const newFiber = {
+      type: child.type,
+      props: child.props,
+      child: null,
+      sibling: null,
+      parent: fiber,
+      dom: null,
+    };
+
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else {
+      prevChild.sibling = newFiber;
+    }
+
+    prevChild = newFiber;
+  });
+}
+
+function performfiberOfUnit(fiber) {
+  if (!fiber.dom) {
+    const dom = (fiber.dom = createDom(fiber.type));
+
+    fiber.parent.dom.append(dom);
+
+    updateProps(dom, fiber.props);
+  }
+
+  // Step3 DOM 树转换链表
+  initChildren(fiber);
+
+  // Step4 返回下一个任务
+  if (fiber.child) return fiber.child;
+  if (fiber.sibling) return fiber.sibling;
+  return fiber.parent?.sibling;
+}
+
+requestIdleCallback(fiberLoop);
 
 const React = {
   render,
